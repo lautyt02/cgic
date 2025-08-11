fc57226064a6d91fcebc1bf1b98f3e735bf04754
LautyT02 <lautyt02@gmail.com>
2025-08-11 10:31:29 -0300

    Installed react-router-dom and worked on navitem component

---
+++ src/components/layout/components/navbar/components/navitem/navitem.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+const NavItem = ({ route, name, subitems }) => {
+    return <Link to={route}>
+        <div className="">
+
+        </div>
+    </Link>
+
+}
