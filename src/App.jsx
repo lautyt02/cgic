4a91b1a3b1257e99cf5e5c8412a89754bb062f02
LautyT02 <lautyt02@gmail.com>
2025-08-11 20:05:26 -0300

    Imported pages and added React Router Dom support in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import { Layout } from './components'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Projects, HubDeInovacion } from './pages'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Layout>
-        Children
-      </Layout>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path='/projects/' element={Projects} />
+            <Route path='/projects/hub-de-inovacion' element={HubDeInovacion} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+
     </>
   )
 }
