69c0962cfc664baf19a01d98e36b6997c155829d
LautyT02 <lautyt02@gmail.com>
2025-08-17 17:52:53 -0300

    Changed the way Form component generates its children components

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,14 +1,36 @@
 import { FormButton, Input, TextArea, Radius, Select } from "./components"
-const Form = ({ title, action, method, inputs, buttons }) => {
+const Form = ({ title, action, method, elements }) => {
+    const inputGenerator = element => {
+        switch (element.elType) {
+            case "input":
+                return  <Input id={element.atributes.id} label={element.atributes.label} type={element.atributes.type} name={element.atributes.name || ""} value={element.atributes.value || ""} placeholder={element.atributes.placeholder || ""} autocomplete={element.atributes.autocomplete || false} />
+            case "button":
+                return <FormButton id={element.atributes.id} value={element.atributes.value} handleOnClick={element.atributes.onClick} />
+            case "textarea":
+                return <TextArea  {...element.atributes} />
+            case "radius":
+                return <Radius  {...element.atributes} />
+            case "select":
+                return <Select  {...element.atributes} />
+            default:
+                return <h7>Not a valid element type</h7>
+        }
+
+    }
     return <>
-        <h4>{title}</h4>     <form action={action} method={method} className="">
-            {inputs && inputs.map(input => {
-                return <Input id={input.id} label={input.label} type={input.type} name={input.name || ""} value={input.value || ""} placeholder={input.placeholder || ""} autocomplete={input.autocomplete || false} />
-            })}
-            {buttons && buttons.map(button => {
-                return <FormButton id={button.id} value={button.value} handleOnClick={button.onClick} />
+        <h4>{title}</h4>
+        <form action={action} method={method} className="">
+            {elements.map((element,i) => {
+                return <div key={element.atributes.id + i}>{inputGenerator(element)} </div>
             })}
         </form>
     </>
 }
 export { Form }
+
+// {inputs && inputs.map(input => {
+//                 return <Input id={input.id} label={input.label} type={input.type} name={input.name || ""} value={input.value || ""} placeholder={input.placeholder || ""} autocomplete={input.autocomplete || false} />
+//             })}
+//             {buttons && buttons.map(button => {
+//                 return <FormButton id={button.id} value={button.value} handleOnClick={button.onClick} />
+//             })}
--- src/pages/projects/hub-de-inovacion/hub-de-inovacion.jsx
+++ src/pages/projects/hub-de-inovacion/hub-de-inovacion.jsx
@@ -4,11 +4,11 @@ const paragraphs = [
     { id: "que-va-a-haber", title: "¿Qué va a haber?", text: "hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub" },
     { id: "cuando", title: "¿Cuando es?", text: "hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub" }
 ]
-const inputs = [{ id: "nombre", label: "Nombre:", type: "text", name: "nombre", placeholder: "Ingrese su nombre" },
-    { id: "apellido", label: "Apellido:", type: "text", name: "apellido", placeholder: "Ingrese su apellido" },
-    { id: "nombre-institucion", label: "Institución de la que forma parte:", type: "text", name: "nombre-institucion", placeholder: "Ingrese el nombre de la Institución" },
-    { id: "tel", label: "Teléfono:", type: "tel", name: "tel", placeholder: "Ingrese su numero de teléfono" },
-    { id: "mail", label: "Mail:", type: "email", name: "mail", placeholder: "Ingrese su dirección de mail" },
+const elements = [{elType: "input", atributes:{ id: "nombre", label: "Nombre:", type: "text", name: "nombre", placeholder: "Ingrese su nombre" }},
+    {elType: "input", atributes:{ id: "apellido", label: "Apellido:", type: "text", name: "apellido", placeholder: "Ingrese su apellido" }},
+    {elType: "input", atributes:{ id: "nombre-institucion", label: "Institución de la que forma parte:", type: "text", name: "nombre-institucion", placeholder: "Ingrese el nombre de la Institución" }},
+    {elType: "input", atributes:{ id: "tel", label: "Teléfono:", type: "tel", name: "tel", placeholder: "Ingrese su numero de teléfono" }},
+    {elType: "input", atributes:{ id: "mail", label: "Mail:", type: "email", name: "mail", placeholder: "Ingrese su dirección de mail" }},
 ]
 const buttons = []
 
@@ -19,7 +19,7 @@ const HubDeInovacion = () => {
             {paragraphs && paragraphs.map((paragraph) => {
                 return <Paragraph id={paragraph.id} key={paragraph.id} title={paragraph.title} text={paragraph.text} />
             })}
-            <Form inputs={inputs}>
+            <Form elements={elements}>
 
             </Form>
 
