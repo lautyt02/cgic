2c4a0e16ce7ca62c176af8b3db01e19a3c63003e
LautyT02 <lautyt02@gmail.com>
2025-08-16 22:22:27 -0300

    Tried Form component inside Hub de Inovacion page

--- src/pages/projects/hub-de-inovacion/hub-de-inovacion.jsx
+++ src/pages/projects/hub-de-inovacion/hub-de-inovacion.jsx
@@ -1,9 +1,17 @@
 import { Paragraph, Form } from "../../../components"
 const paragraphs = [
-    {id:"quienes-somos" ,title:"¿Quienes Somos?", text:"hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub"},
-    {id:"que-va-a-haber" ,title:"¿Qué va a haber?", text:"hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub"},
-    {id:"cuando" ,title:"¿Cuando es?", text:"hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub"}
+    { id: "quienes-somos", title: "¿Quienes Somos?", text: "hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub" },
+    { id: "que-va-a-haber", title: "¿Qué va a haber?", text: "hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub" },
+    { id: "cuando", title: "¿Cuando es?", text: "hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub hbdhbshbsd dshbshdbbhdsbhsd sdnsdjnjdsjnds kmsdndisbfub" }
 ]
+const inputs = [{ id: "nombre", label: "Nombre:", type: "text", name: "nombre", placeholder: "Ingrese su nombre" },
+    { id: "apellido", label: "Apellido:", type: "text", name: "apellido", placeholder: "Ingrese su apellido" },
+    { id: "nombre-institucion", label: "Institución de la que forma parte:", type: "text", name: "nombre-institucion", placeholder: "Ingrese el nombre de la Institución" },
+    { id: "tel", label: "Teléfono:", type: "tel", name: "tel", placeholder: "Ingrese su numero de teléfono" },
+    { id: "mail", label: "Mail:", type: "email", name: "mail", placeholder: "Ingrese su dirección de mail" },
+]
+const buttons = []
+
 const HubDeInovacion = () => {
 
     return <>
@@ -11,7 +19,9 @@ const HubDeInovacion = () => {
             {paragraphs && paragraphs.map((paragraph) => {
                 return <Paragraph id={paragraph.id} key={paragraph.id} title={paragraph.title} text={paragraph.text} />
             })}
-        <Form></Form>
+            <Form inputs={inputs}>
+
+            </Form>
 
         </section>
         <section>
