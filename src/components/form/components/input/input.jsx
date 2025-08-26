c155cba05776859f9becc48ad3b7bef2c7290b41
LautyT02 <lautyt02@gmail.com>
2025-08-26 09:18:47 -0300

    Solved error with value and setValue inside input.jsx component

--- src/components/form/components/input/input.jsx
+++ src/components/form/components/input/input.jsx
@@ -3,8 +3,8 @@ import { useState } from "react"
 // component for inputs of type: Text, number, email, password and url
 const validTypes = ["text", "number", "email", "password", "url"]
 
-const Input = ({ id, label, type, name, value, placeholder, autocomplete }) => {
-    [value,setValue]=useState(value)
+const Input = ({ id, label, type, name, propValue, placeholder, autocomplete }) => {
+    const [value,setValue]=useState(propValue)
     autocomplete = type == "pasword" ? "false" : autocomplete
     return <>
         <label htmlFor={id}>
