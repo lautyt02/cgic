9d427bd5255ff7b09d53462b15ab48d236d0b555
LautyT02 <lautyt02@gmail.com>
2025-08-26 09:00:43 -0300

    Solved error with autocomplete atribute. Change it to autoComplete

--- src/components/form/components/input/input.jsx
+++ src/components/form/components/input/input.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
+
 // component for inputs of type: Text, number, email, password and url
 const validTypes = ["text", "number", "email", "password", "url"]
 
 const Input = ({ id, label, type, name, value, placeholder, autocomplete }) => {
-    autocomplete = type == "pasword" ? false : autocomplete
+    [value,setValue]=useState(value)
+    autocomplete = type == "pasword" ? "false" : autocomplete
     return <>
         <label htmlFor={id}>
             {label}
-            <input className="" id={id} type={type} name={name} value={value} placeholder={placeholder} autocomplete={autocomplete} />
+            <input className="" id={id} type={type} name={name} value={value} placeholder={placeholder} autoComplete={autocomplete||"false"} />
         </label>
 
     </>
