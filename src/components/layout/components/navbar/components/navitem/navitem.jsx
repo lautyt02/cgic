3b73df936c17bae0fd71df709e0b4887406a45b2
LautyT02 <lautyt02@gmail.com>
2025-09-05 12:46:17 -0300

    Imported NavHashLink Component and used it in NavItem Component

--- src/components/layout/components/navbar/components/navitem/navitem.jsx
+++ src/components/layout/components/navbar/components/navitem/navitem.jsx
@@ -1,13 +1,10 @@
-import { useNavigate } from "react-router-dom"
-const NavItem = ({ route, name, children }) => {
-    let navigateTo=useNavigate()
-    const onClickHandle = ()=>{
-        navigateTo(route)
-    }
-    return <div className="text-cgic-naranja-1 text-2xl font-bold px-2  min-h-20 h-full hover:text-cgic-azul hover:bg-gray-300 hover:text- hover:font-extrabold items-center" onClick={onClickHandle}>
-        <h5>{name}</h5>
-        {children}
-    </div>
+import { NavLink } from "react-router-dom"
+import { NavHashLink } from "react-router-hash-link/dist/react-router-hash-link.cjs.development"
+const NavItem = ({ route, name}) => {
 
+    return <NavHashLink className="text-cgic-naranja-1 text-2xl font-bold px-2  h-full hover:text-cgic-azul hover:bg-gray-300  hover:font-extrabold" to={route} smooth>
+        <h3 className="m-6">{name}</h3>
+    </NavHashLink>
+   
 }
 export { NavItem }
