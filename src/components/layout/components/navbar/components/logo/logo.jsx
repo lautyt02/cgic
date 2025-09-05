0d0d8217c670703d020c9c3ae1cd94ea4dbb4811
LautyT02 <lautyt02@gmail.com>
2025-09-05 20:48:03 -0300

    Corrected src and url atributes where needed due to changes in assets directory

--- src/components/layout/components/navbar/components/logo/logo.jsx
+++ src/components/layout/components/navbar/components/logo/logo.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom"
 import { NavHashLink } from "react-router-hash-link/dist/react-router-hash-link.cjs.development"
 const Logo=()=>{
     return <NavHashLink to="/#">
-        <img className="objetc-cover w-35 flex-none my-2" src="/src/assets/logo-cgic.svg" alt="logo" />
+        <img className="objetc-cover w-35 flex-none my-2" src="/src/assets/images/logo-cgic.svg" alt="logo" />
     </NavHashLink>
     
 }
