4fc6d5b003077923ed69ac9644add8604363b08e
LautyT02 <lautyt02@gmail.com>
2025-09-05 11:42:30 -0300

    Working on navbar styles

--- src/components/layout/components/navbar/components/logo/logo.jsx
+++ src/components/layout/components/navbar/components/logo/logo.jsx
@@ -1,12 +1,12 @@
 import {Link} from "react-router-dom"
 const Logo=()=>{
     return <Link to="/">
-        <img className="objetc-cover w-[7dvw]" src="/src/assets/logo-cgic.svg" alt="logo" />
+        <img className="objetc-cover w-35 flex-none my-2" src="/src/assets/logo-cgic.svg" alt="logo" />
     </Link>
     
 }
 export {Logo}
 // bg-[url('/src/assets/cgic-logo.png')]
 
-<div className="bg-[url(/src/assets/logo-cgic.svg)] bg-no-repeat bg-cover min-w-3xs min-h-[20px] m-10" >
+<div className="bg-[url(/src/assets/logo-cgic.svg)] flex-none bg-no-repeat bg-cover w-3xs h-[20px]" >
     </div>
--- src/components/layout/components/navbar/components/navitem/navitem.jsx
+++ src/components/layout/components/navbar/components/navitem/navitem.jsx
@@ -4,7 +4,7 @@ const NavItem = ({ route, name, children }) => {
     const onClickHandle = ()=>{
         navigateTo(route)
     }
-    return <div className="text-cgic-naranja-1 text-2xl font-bold p-2 h-full hover:text-cgic-azul hover:bg-gray-300 hover:text- hover:font-extrabold" onClick={onClickHandle}>
+    return <div className="text-cgic-naranja-1 text-2xl font-bold px-2  min-h-20 h-full hover:text-cgic-azul hover:bg-gray-300 hover:text- hover:font-extrabold items-center" onClick={onClickHandle}>
         <h5>{name}</h5>
         {children}
     </div>
