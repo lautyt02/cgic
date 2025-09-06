a5f912d739c5033b97ccf8103b34553df944f800
LautyT02 <lautyt02@gmail.com>
2025-09-05 21:49:23 -0300

    Increased Header component text size

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
 const Header =({text})=>{
-    return <h2 className="font-extrabold text-cgic-naranja-1 mb-2">{text}</h2>
+    return <h2 className="text-xl font-extrabold text-cgic-naranja-1 mb-2">{text}</h2>
 }
 export {Header}
