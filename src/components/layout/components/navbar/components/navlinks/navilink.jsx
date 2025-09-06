5d910a3c322837447c0a1ee705b62e6e03157d65
LautyT02 <lautyt02@gmail.com>
2025-09-06 00:37:40 -0300

    Made the navbar responsive on mobile screens

--- src/components/layout/components/navbar/components/navlinks/navilink.jsx
+++ src/components/layout/components/navbar/components/navlinks/navilink.jsx
@@ -1,8 +1,8 @@
 import { NavHashLink } from "react-router-hash-link/dist/react-router-hash-link.cjs.development"
 const NavLink = ({ route, name}) => {
 
-    return <NavHashLink className="text-cgic-naranja-1 text-2xl font-bold px-2  h-full hover:text-cgic-azul hover:bg-gray-300  hover:font-extrabold" to={route} smooth>
-        <h3 className="m-6">{name}</h3>
+    return <NavHashLink className="text-cgic-naranja-1 text-2xl font-bold px-2 content-center h-full hover:text-cgic-azul hover:bg-gray-300  hover:font-extrabold" to={route} smooth>
+        <h3 className="text-center">{name}</h3> 
     </NavHashLink>
    
 }
--- src/components/layout/components/navbar/components/navlinks/navlink-container.jsx
+++ src/components/layout/components/navbar/components/navlinks/navlink-container.jsx
@@ -1,13 +1,31 @@
 import { NavLink } from "./navilink"
-const NavLinkContainer = ({items})=>{
-    return <div className="w-4/5 flex flex-row h-full">
+import { useState } from "react"
+const NavLinkContainer = ({ items }) => {
+    const [isSmallScreen, setIsSamallScreen] = useState(false)
+    const [menuIsOpened, setMenuIsOpened] = useState(false)
+    const isOpenedClasses = "pt-20 w-dvw h-dvh absolute top-0 left-0 flex-col z-3 bg-cgic-gris-claro align-items-center flex-col"
+    const isNotOpenedClasses = "w-4/5  flex-row h-full hidden md:flex"
+    const isOpenSrc = "/src/assets/icons/close.svg"
+    const isNotOpenSrc = "/src/assets/icons/hamburguer-menu.svg"
+    const togleIsOpened = () => {
+        setMenuIsOpened(!menuIsOpened)
+    }
+    if (isSmallScreen) {
+        return <></>
+    }
+    else {
+        return <>
+            <div className={menuIsOpened?isOpenedClasses:isNotOpenedClasses} onClick={()=> setMenuIsOpened(false)}>
                 {items && items.map((item, i) => {
-                const key = item.name + i
-                console.log(key)
-                i++
-                return <NavLink key={key} route={item.route} name={item.name} />
-            })}
+                    const key = item.name + i
+                    console.log(key)
+                    i++
+                    return <NavLink key={key} route={item.route} name={item.name} />
+                })}
             </div>
+            <button className="z-4 w-15 mr-3 md:hidden" onClick={togleIsOpened}><img src={menuIsOpened ? isOpenSrc : isNotOpenSrc} alt="" /></button>
+        </>
+    }
 }
-export {NavLinkContainer}
+export { NavLinkContainer }
 
