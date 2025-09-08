caa49a7abfcb6f6e79ee5371ff4c1027908e3e9f
LautyT02 <lautyt02@gmail.com>
2025-09-08 12:27:43 -0300

    Worked on navbar styles

--- src/components/layout/components/navbar/components/logo/logo.jsx
+++ src/components/layout/components/navbar/components/logo/logo.jsx
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom"
 import { NavHashLink } from "react-router-hash-link/dist/react-router-hash-link.cjs.development"
 const Logo=()=>{
     return <NavHashLink className="w-1/5 min-w-27" to="/#">
-        <img className="ml-2 objetc-cover h-20 m-auto col-span-1" src="/src/assets/images/logo-cgic.svg" alt="logo" />
+        <img className="objetc-cover h-20 m-auto col-span-1" src="/src/assets/images/logo-cgic.svg" alt="logo" />
     </NavHashLink>
     
 }
