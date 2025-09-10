2c0b39090d308d0fbcf550e34ef71573d0cda490
LautyT02 <lautyt02@gmail.com>
2025-09-10 03:50:08 -0300

    Styled Paragraph Component

--- src/components/paragraph/paragraph.jsx
+++ src/components/paragraph/paragraph.jsx
@@ -3,7 +3,7 @@ const Paragraph = ({ title, text, id }) => {
 
     return <div id={id}className="mb-5">
         <Header  text={title}/>
-        <p className="">{text}</p>
+        <p className="text-justify text-balance align-middle">{text}</p>
     </div>
 }
 export {Paragraph}
