8c8cf0895b62c95093ec6ba377efb09b19c5c533
LautyT02 <lautyt02@gmail.com>
2025-10-14 16:42:12 -0300

    Changed Styles in NavLink component

--- frontend/src/components/layout/components/navbar/components/navlinks/navilink.tsx
+++ frontend/src/components/layout/components/navbar/components/navlinks/navilink.tsx
@@ -5,8 +5,8 @@ type NavLinkProps = {
 }
 const NavLink = ({ route, name }: NavLinkProps) => {
 
-    return <NavHashLink className="text-cgic-naranja-1 text-2xl font-bold px-2 content-center h-full hover:text-cgic-azul hover:bg-gray-300  hover:font-extrabold" to={route} smooth>
-        <h3 className="text-center">{name}</h3>
+    return <NavHashLink className="block lg:text-xl text-center text-cgic-naranja-1 md:flex-1 font-bold py-5 md:py-0 px-2 content-center hover:text-cgic-azul hover:bg-gray-300  hover:font-extrabold md:min-h-1/2 " to={route} smooth>
+        {name}
     </NavHashLink>
 
 }
