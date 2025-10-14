2e5896f34e7db89f722fc3e241deeed63e5b04f9
LautyT02 <lautyt02@gmail.com>
2025-10-14 16:43:40 -0300

    Changed Styles and removed testing console.log()

--- frontend/src/components/layout/components/navbar/components/navlinks/navlink-container.tsx
+++ frontend/src/components/layout/components/navbar/components/navlinks/navlink-container.tsx
@@ -10,8 +10,8 @@ type NavLinkContainerProps = {
 const NavLinkContainer = ({ items }: NavLinkContainerProps) => {
 
     const [menuIsOpened, setMenuIsOpened] = useState(false)
-    const isOpenedClasses = "pt-20 w-dvw h-dvh absolute top-0 left-0 flex-col z-3 bg-cgic-gris-claro align-items-center flex-col"
-    const isNotOpenedClasses = "w-4/5  flex-row h-full hidden md:flex"
+    const isOpenedClasses = "pt-25 w-dvw h-dvh absolute top-0 left-0 z-3 bg-cgic-gris-claro flex-col justify-items-strech"
+    const isNotOpenedClasses = "mx-5 lg:mx-10 w-full  flex-row  hidden md:flex items-strech flex-wrap h-25"
     const isOpenSrc = "/src/assets/icons/close.svg"
     const isNotOpenSrc = "/src/assets/icons/hamburguer-menu.svg"
     const togleIsOpened = () => {
@@ -21,12 +21,11 @@ const NavLinkContainer = ({ items }: NavLinkContainerProps) => {
         <div className={menuIsOpened ? isOpenedClasses : isNotOpenedClasses} onClick={() => setMenuIsOpened(false)}>
             {items && items.map((item, i) => {
                 const key = item.name + i
-                console.log(key)
                 i++
                 return <NavLink key={key} route={item.route} name={item.name} />
             })}
         </div>
-        <button className="z-4 w-12 md:hidden" onClick={togleIsOpened}><img src={menuIsOpened ? isOpenSrc : isNotOpenSrc} alt="Abrir o cerrar menu" /></button>
+        <button className="z-4 w-12 md:hidden mr-3" onClick={togleIsOpened}><img src={menuIsOpened ? isOpenSrc : isNotOpenSrc} alt="Abrir o cerrar menu" /></button>
     </>
 }
 export { NavLinkContainer }
