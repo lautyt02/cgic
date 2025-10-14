02ff2bce0eafcce9d4ba088936490f2d3e49511a
LautyT02 <lautyt02@gmail.com>
2025-10-14 16:36:01 -0300

    Change import for NavHashLink component and removed Link import

--- frontend/src/components/layout/components/navbar/components/logo/logo.tsx
+++ frontend/src/components/layout/components/navbar/components/logo/logo.tsx
@@ -1,5 +1,4 @@
-import {Link} from "react-router-dom"
-import { NavHashLink } from "react-router-hash-link/dist/react-router-hash-link.cjs.development"
+import { NavHashLink } from "react-router-hash-link"
 const Logo=()=>{
     return <NavHashLink className="min-w-27 py-3" to="/#">
         <img className="objetc-cover h-19 col-span-1" src="/src/assets/images/logo-cgic.svg" alt="logo" />
