228d5313dde9006d2ac74e8bd9e38d33a0d2c310
LautyT02 <lautyt02@gmail.com>
2025-10-14 16:34:10 -0300

    Added missing links in the navbar and changed some taildwind styles

--- frontend/src/components/layout/components/navbar/navbar.tsx
+++ frontend/src/components/layout/components/navbar/navbar.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react"
 import { NavLinkContainer, Logo } from "./components"
 const items = [
-    { name: "¿Quienes somos?", route: "/projects/hub-de-inovacion#quienes-somos", subitems: [{ route: "/projects/hub-de-inovacion", name: "subitem 1" }, { route: "/projects/hub-de-inovacion", name: "subitem 2" }, { route: "/projects/hub-de-inovacion", name: "subitem 3" }, { route: "/projects/hub-de-inovacion", name: "subitem 4" }, { route: "/projects/hub-de-inovacion", name: "subitem 5" }] },
-    { name: "¿Qué es el Hub?", route: "/projects/hub-de-inovacion#", subitems: [{ route: "/projects/hub-de-inovacion", name: "subitem 1" }, { route: "/projects/hub-de-inovacion", name: "subitem 2" }, { route: "/projects/hub-de-inovacion", name: "subitem 3" }, { route: "/projects/hub-de-inovacion", name: "subitem 4" }] },
-    { name: "¿Qué va a haber?", route: "/projects/hub-de-inovacion#que-va-a-haber", subitems: [{ route: "/projects/hub-de-inovacion", name: "subitem 1" }, { route: "/projects/hub-de-inovacion", name: "subitem 2" }, { route: "/projects/hub-de-inovacion", name: "subitem 3" }, { route: "/projects/hub-de-inovacion", name: "subitem 4" }, { route: "/projects/hub-de-inovacion", name: "subitem 5" }] }
+    { name: "Quienes somos", route: "/projects/hub-de-inovacion#quienes-somos"},
+    {name: "Nuestra Propuesta", route:"/projects/hub-de-inovacion#nuestra-propuesta"},
+    { name: "Antecedentes", route: "/projects/hub-de-inovacion#antecedentes"},
+    { name: "¿Por qué es importante?", route: "/projects/hub-de-inovacion#por-que"},
+    { name: "Contactanos", route: "/projects/hub-de-inovacion#hub-contact-form" }
 ]
+
 const NavBar = () => {
     return <>
-        <nav className="flex bg-cgic-gris-claro h-25 items-center justify-between shadow-xs shadow-cgic-azul fixed top-0 left-0 right-0 w-dvw px-3">
+        <nav className="flex bg-cgic-gris-claro h-25 items-center justify-between shadow-xs shadow-cgic-azul fixed top-0 left-0 right-0 w-dvw pl-3">
             <Logo />
             <NavLinkContainer items={items}/>
         </nav>
