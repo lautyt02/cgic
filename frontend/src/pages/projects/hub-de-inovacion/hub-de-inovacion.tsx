b11a3ff12e83a7037f1c8c911d44567afd945d9a
LautyT02 <lautyt02@gmail.com>
2025-10-17 22:15:29 -0300

    Temporarily moved data to hub-de-innovacion.tsx so it is easier to access

--- frontend/src/pages/projects/hub-de-inovacion/hub-de-inovacion.tsx
+++ frontend/src/pages/projects/hub-de-inovacion/hub-de-inovacion.tsx
@@ -1,35 +1,95 @@
-import { useEffect, useState } from "react"
 import { Paragraph, Banner } from "../../../components"
 import { HubForm } from "./components"
-import { getLogos, getHubData } from "../../../servicies/content"
+import type { Tlogos, Tparagraph } from "../../../data/data-types"
 
 const HubDeInovacion = () => {
-    const [logos, setLogos] = useState([])
-    const [paragraphs, setParagraphs] = useState([])
-    useEffect(() => {
-        getHubData().then((data) => {
-            setParagraphs(data)
-        })
-        getLogos().then((data) => {
-            setLogos(data)
-        })
-    }, [])
-    const paddingX="px-[10dvw]"
+    const paragraphs: Tparagraph[] = [
+        {
+            id: "quienes-somos", title: "Quienes Somos",
+            text: <>Somos el <b>Centro de Gestión Integral de Crisis</b> (CGIC), un espacio de articulación que coordina esfuerzos entre distintos actores para dar respuestas eficientes y sostenidas frente a crisis climáticas y emergencias socioambientales. Promovemos el trabajo colaborativo, la optimización de recursos y la construcción de soluciones integrales adaptadas a cada territorio. Nuestro objetivo es transformar la gestión de riesgos en una práctica habitual, replicable y centrada en la resiliencia comunitaria. </>
+        },
+        {
+            id: "nuestra-propuesta", title: "Nuestra Propuesta",
+            text: <>A través de un enfoque participativo e interdisciplinario, el proyecto <b>Hub de Innovación Climática y Hábitat</b> busca impulsar en Luján de Cuyo un espacio de co-creación conformado por jóvenes de distintas disciplinas, orientado al diseño de un prototipo de “Refugio Bioclimático Comunitario”. Este diseño de prototipo se desarrollará a partir de sistemas constructivos modernos ya testeados, y se complementarán con el estudio de materiales tecnológicos innovadores, teniendo en cuenta las características específicas del territorio local.
+                <br /><br />
+                Proponemos ampliar esta iniciativa a otros sectores de la provincia, profundizando el enfoque interdisciplinario y fortaleciendo la construcción colectiva y colaborativa del conocimiento.</>
+        },
+        {
+            id: "antecedentes", title: "Antecedentes",
+            text: <>El municipio de  Luján de Cuyo es un territorio con diversidad geográfica —llanuras, piedemonte y alta montaña— y clima cálido-templado, con escasas precipitaciones y grandes amplitudes térmicas, factores que condicionan la calidad de vida local. El Plan de Acción Climática (PLAC) <b>identifica riesgos de alto impacto</b> como vientos fuertes y Zonda, avalanchas, deslaves, tormentas, granizo, sequía, incendios forestales y olas de frío. Estas amenazas, al combinarse, potencian sus consecuencias.
+                <br /><br />
+                En un contexto de cambio climático, el aumento de la temperatura global ya alcanza 1,1 °C y podría llegar a 1,5 °C entre 2030 y 2035, intensificando fenómenos extremos y provocando pérdidas irreversibles (Fundación Vida Silvestre, 2025). Según la ONU (2023), entre 2010 y 2020 las regiones más vulnerables registraron <b>tasas de mortalidad 15 veces superiores por desastres</b>. Por ello, desarrollar modelos constructivos resilientes y adaptados localmente es clave para proteger la seguridad y la calidad de vida.</>
+        },
+        {
+            id: "por-que", title: "¿Por qué es importante abordar esta temática?",
+            text: <>Frente al escenario global del cambio climático y sus impactos cada vez más visibles a nivel local, se vuelve urgente pensar estrategias que fortalezcan la resiliencia de nuestras comunidades. En territorios como las periferias de Luján de Cuyo, donde las condiciones climáticas extremas y el aumento de vulnerabilidad socioambiental en zonas de expansión urbana no planificada se combinan, resulta clave desarrollar soluciones concretas y adaptadas.
+                <br /><br />
+                En este marco, surge el proyecto como una colaboración entre la <b>Municipalidad de Luján de Cuyo y el Fondo Juventud y Acción Climática, creado por Bloomberg Philanthropies,</b> con el objetivo de diseñar y prototipar un refugio bioclimático comunitario, facilitando sistematizar una estructura de trabajo replicable en otros municipios del país y del mundo.
+                <br /><br />
+                Desde el inicio, este proyecto se apoya en la <b>interdisciplinariedad como una fortaleza clave</b> para abordar problemáticas complejas y multicausales. Creemos firmemente en el valor de la diversidad de miradas y saberes para construir soluciones más integrales y sostenibles.
+            </>
+        }
+    ]
+    const logos: Tlogos = [
+        {
+            src: "/src/assets/images/BP_Youth_Climate_Action_Fund_Logo_Options_040824_V1_FNL_YCAF-Logo-with-Tagline-as-Outlined-Text.png",
+            alt: "Logo Youth CLiamate Action Fund",
+            width: "50",
+            link: ""
+        },
+        {
+            src:"/src/assets/images/united-cities-and-local-goverments.png",
+            alt:"Logo United Cities and Local Goverments",
+            width:"",
+            link:""
+        },
+        {
+            src:"/src/assets/images/bloomberg-center-for-public-innovation.png",
+            alt:"Logo Blomberg Center for Public Innovation",
+            width:"50",
+            link:""
+        },
+        {
+            src:"/src/assets/images/lujan-sustentable.png",
+            alt:"Logo Luján Sustentable",
+            width:"",
+            link:""
+        },
+        {
+            src: "/src/assets/images/logo-escuela-de-steel-framing.webp",
+            alt: "Logo Escuela de Steel Framing",
+            width: "25",
+            link: ""
+        },
+        {
+            src: "/src/assets/images/logo-extra-brut.webp",
+            alt: "Logo Grupo ExtraBrut",
+            width: "25",
+            link: ""
+        },
+        {
+            src: "/src/assets/images/logo-fundacion-lupa.png",
+            alt: "Logo Fundación Lupa",
+            width: "25",
+            link: ""
+        }
+    ]
+    const paddingX = "px-[10dvw]"
     return <>
         <main >
             <section className={paddingX}>
                 {paragraphs && paragraphs.map((paragraph) => {
                     return <Paragraph id={paragraph.id} key={paragraph.id} title={paragraph.title} text={paragraph.text} />
                 })}
-                
+
             </section>
-            <section className={"mb-5 "+`md:${paddingX}`}>
+            <section className={"mb-5 " + "md:px-[10dvw]"}>
                 <HubForm />
             </section>
             <section className={`md:${paddingX}`}>
                 <Banner title="Nos Acompañan" id="sponsors" images={logos} />
             </section>
-            
+
         </main>
     </>
 }
